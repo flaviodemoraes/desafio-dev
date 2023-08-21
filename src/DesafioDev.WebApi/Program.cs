4f30cd9e5ad679b82cb5cf93828b3a9555770eec
Flávio de Moraes <flaviodemoraes@gmail.com>
2023-08-21 14:27:35 -0300

    Configurações das chamadas de serviços e aplicação dos pacotes.

--- src/DesafioDev.WebApi/Program.cs
+++ src/DesafioDev.WebApi/Program.cs
@@ -1,6 +1,39 @@
+using DesafioDev.Operacoes.Infra;
+using DesafioDev.WebApi.Endpoints;
+using DesafioDev.WebApi.Extensions;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<OperacoesDbContext>(options =>
+{
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 0)));
+});
+
+builder.SetSwaggerGenDefinitions();
+
+builder.AddDependencies();
+
+builder.Services.AddCors();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors(cfg =>
+{
+    cfg.AllowAnyHeader();
+    cfg.AllowAnyMethod();
+    cfg.AllowAnyOrigin();
+});
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "CNAB Api v1");
+});
+
+app.MapAppEndpoints();
 
 app.Run();
