63742b103cd54d2194bf90b89bb8cab13257ab8e
Flávio de Moraes <flaviodemoraes@gmail.com>
2023-08-21 14:19:04 -0300

    Criação dos mapetamento entre entidade e viewmodels.

---
+++ src/DesafioDev.Operacoes.Applications/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DesafioDev.Operacoes.Applications.ViewModel;
+using DesafioDev.Operacoes.Domain.Entities;
+
+namespace DesafioDev.Operacoes.Applications.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Operacao, OperacaoViewModel>();
+            CreateMap<TipoTransacao, TipoTransacaoViewModel>();
+            CreateMap<Loja, LojaViewModel>();
+        }
+    }
+}
---
+++ src/DesafioDev.Operacoes.Applications/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DesafioDev.Operacoes.Applications.ViewModel;
+using DesafioDev.Operacoes.Domain.Entities;
+
+namespace DesafioDev.Operacoes.Applications.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile()
+        {
+            CreateMap<OperacaoViewModel, Operacao>();
+            CreateMap<TipoTransacaoViewModel, TipoTransacao>();
+            CreateMap<LojaViewModel, Loja>();
+        }
+    }
+}
