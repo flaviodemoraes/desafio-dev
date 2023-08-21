c697bdf0615a8bee5a6e49221d7c20be856d49ab
Flávio de Moraes <flaviodemoraes@gmail.com>
2023-08-21 14:09:44 -0300

    Criação do log de infra. O mesmo foi criado apenas para se printado via console.

---
+++ src/DesafioDev.Operacoes.Infra/Logs/ConsoleLogger.cs
@@ -0,0 +1,28 @@
+﻿using DesafioDev.Core.Logs;
+using System.Runtime.CompilerServices;
+
+namespace DesafioDev.Operacoes.Infra.Logs
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void Debug<TContext>(string msg, [CallerMemberName] string memberName = "", [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            Console.WriteLine($"[DEBUG] - {msg} ({typeof(TContext).Name}.{memberName} : {sourceLineNumber}).");
+        }
+
+        public void Error<TContext>(string msg, [CallerMemberName] string memberName = "", [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            Console.WriteLine($"[ERROR] - {msg} ({typeof(TContext).Name}.{memberName} : {sourceLineNumber}).");
+        }
+
+        public void Info<TContext>(string msg, [CallerMemberName] string memberName = "", [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            Console.WriteLine($"[INFO] - {msg} ({typeof(TContext).Name}.{memberName} : {sourceLineNumber}).");
+        }
+
+        public void Warning<TContext>(string msg, [CallerMemberName] string memberName = "", [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            Console.WriteLine($"[WARNING] - {msg} ({typeof(TContext).Name}.{memberName} : {sourceLineNumber}).");
+        }
+    }
+}
